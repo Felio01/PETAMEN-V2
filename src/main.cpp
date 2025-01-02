9991016a8acfc3ccd3d0d11460060b41f039f83b
Felio01 <felio.chano@yahoo.co.id>
2025-01-02 09:40:00 +0700

    Final Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,73 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <PID_v1.h>
 #include <Wifimanager.h>
 #include <PubSubClient.h>
+#include <Adafruit_ADS1X15.h>
+#include <SPI.h>
+#include <Wire.h>
 
-#define VCC               3.3 //Voltage of ESP32
-#define ThermistorPIN     4   //Thermistor is connected to GPIO 4
-#define MSG_BUFFER_SIZE   (50)
+#define VCC 3.264       // Voltage of ESP32
+#define ThermistorPIN 4 // Thermistor is connected to GPIO 4
+#define HeaterPIN 5     // Heater is connected to GPIO 5
+#define stepPin 6       // Stepper motor is connected to GPIO 6
+#define limit1 1
+#define limit2 2
+#define MSG_BUFFER_SIZE (50)
 
-const char* mqtt_server = "172.203.134.8";
+const char *mqtt_server = "172.203.134.8";
 
-float ResistorValue = 100000; // 100k Ohm
-float beta          = 3950;   // Beta factor
-float T0            = 298.15; // 25°C in Kelvin
-float R0            = 100000; // 100k Ohm at 25°C
+float ResistorValue = 4700; // 4k7 Ohm
+float beta = 3950;          // Beta factor
+float T0 = 298.15;          // 25°C in Kelvin
+float R0 = 100000;          // 100k Ohm at 25°C
+
+double setpoint = 220;
+double input, output;
+
+double Kp = 18, Ki = 0, Kd = 0;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-unsigned long lastMsg = 0;
+PID myPID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
+Adafruit_ADS1115 ads;
 
-void reconnect() {
+
+// PWM configuration
+const int pwmChannel = 0;
+const int pwmFrequency = 400;
+const int pwmResolution = 8;
+
+// PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+unsigned long previousMillis = 0;
+const long interval = 1000;
+int16_t adcValue;
+float voltage;
+int motorSpeed = 600;
+
+
+void reconnect()
+{
   // Loop until we're reconnected
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
-    if (client.connect(clientId.c_str())) {
+    if (client.connect(clientId.c_str()))
+    {
       Serial.println("Connected");
       // Once connected, publish an announcement...
       client.publish("iotfrontier/mqtt", "iotfrontier");
       // ... and resubscribe
-      client.subscribe("iotfrontier/mqtt");
-    } else {
+      client.subscribe("testking");
+      client.subscribe("petamen/motorspeed");
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -42,9 +77,46 @@ void reconnect() {
   }
 }
 
-float readTemp(){
-  int adcValue = analogRead(ThermistorPIN);
-  float voltage = (adcValue * VCC) / 4095;
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+  String messageBuffer;
+  Serial.print("Message received on topic: ");
+  Serial.println(topic);
+
+  Serial.print("Message: ");
+  for (unsigned int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+
+    messageBuffer += (char)payload[i];
+  }
+  if (String(topic) == "testking")
+  {
+    setpoint = messageBuffer.toFloat();
+    Serial.print("Setpoint: ");
+    Serial.println(setpoint);
+  }
+
+  else if (String(topic) == "petamen/motorspeed")
+  {
+    motorSpeed = messageBuffer.toInt();
+    Serial.print("Motor speed: ");
+    Serial.println(motorSpeed);
+  }
+
+  Serial.println();
+}
+
+int termNom = 100000; // Thermistor reference resistance
+
+int refTemp = 25;
+
+float current;
+
+float readTemp()
+{
+  adcValue = ads.readADC_SingleEnded(0);
+  voltage = ads.computeVolts(adcValue);
 
   float resistance = ResistorValue * (VCC / voltage - 1);
 
@@ -52,17 +124,18 @@ float readTemp(){
 
   float tempCelsius = tempK - 273.15;
 
-
   return tempCelsius;
 }
 
-void wifiManager(){
+void wifiManager()
+{
 
   WiFiManager wifiManager;
 
   // wifiManager.resetSettings();
 
-  if(!wifiManager.autoConnect("PETAMEN ESP32", "password")) {
+  if (!wifiManager.autoConnect("PETAMEN ESP32", "password"))
+  {
     Serial.println("failed to connect and hit timeout");
     delay(3000);
     ESP.restart();
@@ -73,36 +146,98 @@ void wifiManager(){
   Serial.println(WiFi.localIP());
 }
 
-void temp_display(){
-  float tempCelsius = readTemp();
-  Serial.print("Temperature: ");
-  Serial.print(tempCelsius, 0);
-  Serial.println(" °C");
+void temp_display()
+{
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
+  {
+    previousMillis = currentMillis;
+    Serial.print("ADC Value:");
+    Serial.println(adcValue);
 
-  char tempStr[6];
-  dtostrf(tempCelsius, 4, 0, tempStr);
-  client.publish("petamen/temp", tempStr);
+    Serial.print("Voltage: ");
+    Serial.println(voltage, 5);
+
+    Serial.print("Temperature: ");
+    Serial.print(input, 0);
+    Serial.println(" °C");
+
+    Serial.print("PID Output: ");
+    Serial.println(output);
+
+    char tempStr[6];
+    dtostrf(input, 4, 0, tempStr);
+    client.publish("petamen/temp", tempStr);
+  }
 }
 
-void setup(){
+void stepper()
+{
+  for (int i = 0; i < 200; i++)
+  {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(motorSpeed); // Adjust delay for speed control
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(motorSpeed); // Adjust delay for speed control
+  }
+}
+
+void setup()
+{
   Serial.begin(115200);
   pinMode(ThermistorPIN, INPUT);
+  pinMode(HeaterPIN, OUTPUT);
+  pinMode(stepPin, OUTPUT);
+  pinMode(limit1, INPUT);
 
+  myPID.SetMode(AUTOMATIC);
+  ledcSetup(pwmChannel, pwmFrequency, pwmResolution);
+  ledcAttachPin(HeaterPIN, pwmChannel);
+
+  // Wire.begin(8, 9);
+
+  if (!ads.begin())
+  {
+    Serial.println("ADS1115 tidak terdeteksi!");
+    while (1)
+      ;
+  }
+  Serial.println("ADS1115 berhasil diinisialisasi.");
+
+  // Clear the display buffer
   wifiManager();
 
   client.setServer(mqtt_server, 1883);
+  client.setCallback(mqttCallback);
 
+  reconnect();
 }
 
-void loop(){
+void loop()
+{
 
-  if (!client.connected()) {
+  if (!client.connected())
+  {
     reconnect();
   }
 
   client.loop();
 
-  temp_display();
+  if (limit1 == HIGH)
+  {
+    stepper();
+  }
 
-  delay(1000);
+  input = readTemp();
+
+  myPID.Compute();
+
+  if (output < 20)
+  {
+    output = 20;
+  }
+
+  ledcWrite(pwmChannel, output);
+  stepper();
+  temp_display();
 }
